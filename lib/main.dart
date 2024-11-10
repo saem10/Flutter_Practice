4b3f34d0fb7ca7c4d5e4e7e2e964be8408e8fe7a
saem10 <mirsaemhasan@gmail.com>
2024-11-11 01:50:03 +0600

    GPT code Exam week 2 Assignment

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyProfile(),
+      home: MyProfilePage(),
     );
   }
 }
 
-class MyProfile extends StatelessWidget {
+class MyProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,51 +35,58 @@ class MyProfile extends StatelessWidget {
           ),
         ],
       ),
-
-
       body: Container(
         color: Colors.purple[50],
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 8),
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.purple[100],
-                child: Icon(
-                  Icons.icecream_outlined,
-                  size: 50,
-                  color: Colors.purple[900],
-                ),
-              ),
-              Text('Ice cream is very delicious right?'),
-              SizedBox(height: 16),
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.purple[100],
-                child: Icon(
-                  Icons.code_outlined,
-                  size: 50,
-                  color: Colors.purple[900],
-                ),
-              ),
-              Text('Programming is not boring if you love it'),
-              SizedBox(height: 16),
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.purple[100],
-                child: Icon(
-                  Icons.egg_outlined,
-                  size: 50,
-                  color: Colors.purple[900],
-                ),
-              ),
-              Text('If you submit code directly copy from chatgpt then mark will 0'),
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ProfileItem(
+              icon: Icons.icecream,
+              text: 'Ice cream is very delicious right?',
+            ),
+            ProfileItem(
+              icon: Icons.code,
+              text: 'Programming is not boring if you love it',
+            ),
+            ProfileItem(
+              icon: Icons.warning,
+              text: 'If you submit code directly copy from chatgpt then mark will 0',
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class ProfileItem extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  ProfileItem({required this.icon, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.purple[100],
+            child: Icon(
+              icon,
+              size: 40,
+              color: Colors.purple[900],
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            text,
+            style: TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
