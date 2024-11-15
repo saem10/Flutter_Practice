132b30aa09f2474bc3848858b7629b8045d9d3ec
saem10 <mirsaemhasan@gmail.com>
2024-11-15 22:12:23 +0600

    Live Exam week 2

--- lib/main.dart
+++ lib/main.dart
@@ -8,85 +8,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyProfilePage(),
-    );
-  }
-}
-
-class MyProfilePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text('My Profile'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.call),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Container(
-        color: Colors.purple[50],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ProfileItem(
-              icon: Icons.icecream,
-              text: 'Ice cream is very delicious right?',
-            ),
-            ProfileItem(
-              icon: Icons.code,
-              text: 'Programming is not boring if you love it',
-            ),
-            ProfileItem(
-              icon: Icons.warning,
-              text: 'If you submit code directly copy from chatgpt then mark will 0',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Need Blood'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                // Add button action here
+              },
             ),
           ],
+          backgroundColor: Colors.red,
         ),
-      ),
-    );
-  }
-}
-
-class ProfileItem extends StatelessWidget {
-  final IconData icon;
-  final String text;
-
-  ProfileItem({required this.icon, required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: Column(
-        children: [
-          CircleAvatar(
-            radius: 40,
-            backgroundColor: Colors.purple[100],
-            child: Icon(
-              icon,
-              size: 40,
-              color: Colors.purple[900],
-            ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.grey,
+                child: Icon(
+                  Icons.bloodtype,
+                  color: Colors.red,
+                  size: 50,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Donate Blood',
+                style: TextStyle(fontSize: 18, color: Colors.black),
+              ),
+            ],
           ),
-          SizedBox(height: 8),
-          Text(
-            text,
-            style: TextStyle(fontSize: 16),
-            textAlign: TextAlign.center,
-          ),
-        ],
+        ),
       ),
     );
   }
